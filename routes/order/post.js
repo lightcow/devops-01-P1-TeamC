408ef96fd535a71ecef9a04cc49e5fa22fc7d949
lightcow <joonhyoung10@gmail.com>
2022-03-07 14:00:16 +0900

    4rd commit

--- routes/order/post.js
+++ routes/order/post.js
@@ -7,25 +7,32 @@ module.exports = async function (fastify, opts) {
   fastify.post('/', async (req, reply) => {
     const client = await fastify.pg.connect()
     // const product_idx=req.body.product_idx;
-    const user_id=req.body[0].user_id;
-    const product_id=req.body[1].product_id;
+    const user_id=req.body.user_id;
+    const product_id=req.body.product_id;
     const quantity=req.body.quantity;
-    const order_idx="select max(order_idx) from orders" //마지막 인덱스 찾기
+    //Const order_idx="select max(order_idx) from orders" //마지막 인덱스 찾기
     console.log("user_id",user_id);
     console.log("product_id",product_id);
     console.log("quantity",quantity);
   
+    // const  max= await client.query(
+    //   'select max(order_idx) from orders')
     // console.log("product_idx----------",product_idx)
-    // if(req.body>=2)
-    // {
-    const { rows } = await client.query(
-      'INSERT INTO orders VALUES'+'('+16+','+5+','+0+','+7+','+8+')')
+    //  if(req.body[1]===String)
+    //  {
+      const user_idx = await client.query(
+        "select user_idx from users where id='"+user_id+"'")
+        console.log("=========")
+        console.log(user_idx.rows[0].user_idx);
+    const insert_order = await client.query(
+      'INSERT INTO orders VALUES(default,'+quantity+','+'false'+','+user_idx.rows[0].user_idx+','+product_id+')')
+      const send_req = await client.query()
       //order_idx,quantity,ordered,user_useridx,product_idx
       reply
       .code(200)
       .header('content-type','application/json')
       .send()
-    // }
+     //}
   })
 }
 //다중 sql문 사용법
